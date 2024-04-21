932f6ccc20dd5a08b80b9f05cc7d59365dbda1b7
hasibur14 <hasib141415@gmail.com>
2024-04-21 22:35:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='text-3xl text-purple-600 font-bold '>Coffee Store</h1>
+
+    </>
+  )
+}
+
+export default App
